5642615f522fbbd500fcab4d73b2d2bdeaeb0c80
xrafffcode <yusfiwawan2@gmail.com>
2024-06-04 23:29:02 +0700

    Add user registration functionality

--- storage/framework/views/5be6e6125d59d0d6afd995c60d3d6ab4.php
+++ storage/framework/views/5be6e6125d59d0d6afd995c60d3d6ab4.php
@@ -1,6 +1,4 @@
-@extends('layouts.auth')
-
-@section('content')
+<?php $__env->startSection('content'); ?>
 <div class="col-lg-12">
     <div class="p-5">
         <div class="text-center">
@@ -8,19 +6,19 @@
                 Daftar Medic
             </h1>
         </div>
-        <form class="user" method="POST" action="{{ route('register') }}">
-            @csrf
+        <form class="user" method="POST" action="<?php echo e(route('register')); ?>">
+            <?php echo csrf_field(); ?>
 
             <div class="form-group">
                 <input type="text" class="form-control form-control-user" id="name" aria-describedby="name"
-                    placeholder="Enter Full Name..." name="name" value="{{ old('name') }}" required
+                    placeholder="Enter Full Name..." name="name" value="<?php echo e(old('name')); ?>" required
                     autocomplete="name" autofocus>
             </div>
 
             <div class="form-group">
                 <input type="email" class="form-control form-control-user" id="exampleInputEmail"
                     aria-describedby="emailHelp" placeholder="Enter Email Address..." name="email"
-                    value="{{ old('email') }}" required autocomplete="email" autofocus>
+                    value="<?php echo e(old('email')); ?>" required autocomplete="email" autofocus>
             </div>
             <div class="form-group">
                 <input type="password" class="form-control form-control-user" id="exampleInputPassword"
@@ -33,4 +31,6 @@
         </form>
     </div>
 </div>
-@endsection
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.auth', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /Users/xrafff/Desktop/dilesin/Konsultasi Student/Projek1-main/resources/views/auth/register.blade.php ENDPATH**/ ?>
